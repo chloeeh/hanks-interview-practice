110fbc9da73c3f4b8e4b2390dad8a634c05f20e3
Todey <chloe.e.hanks@gmail.com>
2023-02-16 15:04:35 -0600

    practice prototype chaining

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// What are possible ways to create objects in JS?
+/* ------------------------ [ CREATING OBJECTS ] ------------------------ */
+
 // Object constructor (not recommended)
 var myConstructObject = new Object("Chloe", 28, "student", "hopeless");
 
@@ -13,13 +14,54 @@ var myLiteralObject = {
     occupation: "student",
     wellBeing: "down, but not out",
     description: function() {
-        return `This is ${this.name}. She is ${28}, currently a(n) ${occupation}. Right now she's feeling ${wellBeing}.`
+        return `This is ${this.name}. She is ${this.age}, currently a(n) ${this.occupation}. Right now she's feeling ${this.wellBeing}.`
     }
 };
 
-myConstructObject;
-myCreateObect;
-myLiteralObject.description;
-myLiteralObject.description();
 
+console.log(myConstructObject);                 // [String: 'Chloe]
+console.log(myCreateObect);                     // [Object: null prototype] {}
+console.log(myLiteralObject.description);       // [Function: description]
+console.log(myLiteralObject.description());     // This is Chloe. She is 28, currently a(n) student. Right now she's feeling down, but not out.
+
+
+/* ------------------------ [ PROTOTYPE CHAINING ] ------------------------ */
+
+// Prototype chaining is used to build new types of objects based on existing ones. It is similar to inheritance in a class based language.
+
+// boss -> employee -> person -> object.prototype ->
+
+const boss = {
+    experience: 10,
+    __proto__: {
+        species: "employee",
+        speciality: "tech",
+        wage: 100000,
+        __proto__: {
+            genus: "person",
+            isHuman: true,
+            isAlive: false,
+        },
+    },
+};
+// stringify the whole object otherwise it outputs object Object
+console.log("Only outputs the value specific to boss, which is experience: " + JSON.stringify(boss));
+console.log(`Boss's experience: ${boss.experience}`);
+console.log(`But...is the boss alive? ${boss.isAlive}`);
+
+// stringify the whole object otherwise it outputs object Object. Lists the employee proto
+console.log(`The proto? ${JSON.stringify(boss.__proto__)}`);
+
+console.log(`Boss's species: ${boss.species}`);
+console.log(`Boss's specialty: ${boss.speciality}`);
+console.log(`Boss's wage: ${boss.wage}`);
+
+// stringify the whole object otherwise it outputs object Object. Lists the person proto
+
+console.log(`The proto of the proto? ${JSON.stringify(boss.__proto__.__proto__)}`);
+
+console.log(`The proto? ${JSON.stringify(boss.__proto__)}`);
+console.log(`Boss's genus: ${boss.genus}`);
+console.log(`Is the boss human? ${boss.isHuman}`);
+console.log(`But...is the boss alive? ${boss.isAlive}`);
 
